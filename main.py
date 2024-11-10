e2d79863a61d621d91307656148317b680288a7f
HiralChauhan21 <vishal.chauhan.home@gmail.com>
2024-11-10 14:47:50 +0530

    commit

---
+++ main.py
@@ -0,0 +1,11 @@
+x = input("enter value of x: ")
+y = input("enter value of y: ")
+
+#swapping
+temp = x
+x = y
+y = temp
+
+print("after swapping.....")
+print("value of x: ", x)
+print("value of y: ", y)
